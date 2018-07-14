59303ba20200323654bbb45982d069b6190ece77
SamikshaManglani <samie92@gmail.com>
2018-07-14 13:10:48 +0530

    DESC: Adding Invoice doesn't exist exception

---
+++ restapi/src/main/java/com/pharma/inventory/exception/InvoiceDoesNotExistException.java
@@ -0,0 +1,9 @@
+package com.pharma.inventory.exception;
+
+public class InvoiceDoesNotExistException extends Exception {
+
+	public InvoiceDoesNotExistException(String message) {
+		super(message);
+	}
+
+}
